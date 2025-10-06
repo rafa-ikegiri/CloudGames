4db922d64a73193abd99580bebe541c67cce03ed
Rafael <rafael.ikegiri@gmail.com>
2025-10-06 10:57:26 -0300

    Subindo primeira versão

--- CloudGames/Program.cs
+++ CloudGames/Program.cs
@@ -91,7 +91,6 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
     if (app.Environment.IsDevelopment())
     //if (!app.Environment.IsDevelopment())
     {
